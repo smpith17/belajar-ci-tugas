22812dfa6c81656071d77518db0bcfbea7eec6eb
smpith17 <smpith17@gmail.com>
2025-06-30 17:48:49 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -28,6 +28,12 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+
 $routes->group('kategori', ['filter' => 'auth'], function ($routes) { 
     $routes->get('', 'KategoriController::index');
     $routes->post('', 'KategoriController::create');
