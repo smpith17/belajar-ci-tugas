a7b50b3fde7cc10a6ce779d0d929035bda3a0725
smpith17 <smpith17@gmail.com>
2025-06-30 18:58:28 +0700

    menambahkan api data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -43,6 +43,8 @@ $routes->group('kategori', ['filter' => 'auth'], function ($routes) {
 
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
-$routes->get('faq', 'Home::faq', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+$routes->get('faq', 'Home::faq', ['filter' => 'auth']);
 $routes->get('contact', 'Home::contact', ['filter' => 'auth']);
+
+$routes->resource('api', ['controller' => 'apiController']);
